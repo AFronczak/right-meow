44f641ed65e5e9f3f09f8a7605e91f72c334aa57
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-09-20 15:02:55 -0400

    User and Pets models created.

---
+++ db/migrate/20160920182939_create_pets.rb
@@ -0,0 +1,12 @@
+class CreatePets < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :birthday
+      t.string :type
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
